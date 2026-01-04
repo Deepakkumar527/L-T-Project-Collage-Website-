634bfe4a3a443217494fcc3de415bd59a05cbcf3
Deepakkumar527 <deepakchauhan8o27@gmail.com>
2026-01-04 11:33:30 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,18 @@
+document.getElementById("applyBtn").addEventListener("click", function () {
+  alert("Admissions are open! Please login to apply.");
+});
+
+document.getElementById("loginBtn").addEventListener("click", function () {
+  alert("Login feature will be added soon.");
+});
+
+
+
+Submitted By:
+o Team Members- Full Name
+o Roll Number
+o College-E-mailid
+• Course: e.g., UI/UX Design Fundamentals
+• Instructor Name: e.g., Faculty Name.
+• Institution: e.g., Christ University
+• Date of Submission: DD/MM/YYYY
